55252ec849ff3c047ca3150af8e2302172a5f4d0
ocinpp <ocin25@gmail.com>
2018-09-06 02:39:50 +0800

    Remove the use of deprecated AbstractWebSocketMessageBrokerConfigurer

--- src/main/java/com/example/WebSocketConfig.java
+++ src/main/java/com/example/WebSocketConfig.java
@@ -2,9 +2,9 @@ package com.example;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 /**
  * Configuration to enable websocket 
@@ -12,7 +12,7 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
  */
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
